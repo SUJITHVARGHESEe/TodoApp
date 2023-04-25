be3a13bd11e1210c071ac5c3048c619098566ba0
sujith varghese <sujithvargheseofficial250@gmail.com>
2023-04-25 18:52:54 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const[inputData,setInputData]= useState('')
+  const [todos ,setTodos]=useState([])
+  const [deletInput,setDeletInput]=useState([])
+  const date = new Date();
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayName = days[date.getDay()];
+  
+  const handleDelete=(id)=>{
+   const deleteItems=todos.filter((items)=>items.id!==id)
+   const showDelete= todos.filter((items)=>items.id===id)
+    setTodos(deleteItems)
+    setDeletInput([...deletInput,...showDelete])
+  }
   return (
     <div className="app">
       <div className="mainHeading">
@@ -8,23 +21,69 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's {dayName} 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input type="text" placeholder="🖊️ Add item..."  value={inputData} onChange={(e)=>{setInputData(e.target.value) }}/>
+       {inputData? <i className="fas fa-plus" onClick={(e)=>{setTodos([...todos,{id:date, text:inputData ,status:false}])
+       setInputData('')
+      }}></i>:""}
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+        {todos.map((value)=>{
+          return(
+            <div className="todo">
+              <div className="left">
+                <input type="checkbox" onChange={(e)=>{console.log(e.target.checked)
+                setTodos(todos.map(value2=>{
+                  if(value2.id===value.id){
+                    value2.status=e.target.checked
+                  }
+                  return value2
+                }))
+                }} name="" id="" />
+                <p>{value.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times" onClick={()=>{handleDelete(value.id)}}></i>
+              </div>
+            </div>  
+          )
+        })}
+        
+
+        {todos.some(value => value.status) && (
+          <div>
+            <h3 style={{color:'white',paddingTop:'6px'}}>Active Task</h3>
+            {todos.map((value2)=>{
+              if(value2.status) {
+                return(
+                  <div className="input">
+                    <p>{value2.text}</p>
+                  </div>
+                )
+              }
+              return null
+            })}
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        )}
       </div>
+     {
+      deletInput.length>0 && (
+        <div className="head">
+          <h3 style={{color:'white'}}>Deleted Items</h3>
+        
+      {deletInput.map((value)=>{
+          return(
+            <div className="input">
+              <p style={{textDecoration:"line-through"}}>{value.text}</p>
+              <i className='fas fa-times' onClick={(e)=>setDeletInput(deletInput.filter((items)=>items.id!==value.id))}></i>
+            </div>
+          )
+        })
+      }
+      </div>
+     )}
     </div>
   );
 }
