70bd19e4b2480ebce71ee43d3bf70033ddd6fc37
sujith varghese <sujithvargheseofficial250@gmail.com>
2023-04-25 14:02:40 +0530

    basic structure compleated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      
+      <div className="container">
+        <div className="head">
+          <h1>welcome To</h1>
+          <h1>ToDoApp</h1>
+        </div>
+        <div className="input">
+          <input type="text" placeholder='Add here' />
+          <i class="fas fa-plus"></i>
+        </div>
+        <div className="todos">
+          <div className="todo">
+            <div className="check">
+              <input type="checkbox" />
+              <p>React Tutorial</p>
+            </div>
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
